3eff2ebaa7960ad1ad8163d39e016f5526099dd1
Wally Jay Tech <wallyjay111@gmail.com>
2025-12-02 22:18:26 +0100

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   displayName: { type: String, required: true },
-  linkCode: { type: String, unique: true },
+  linkCode: { type: String, unique: true, default: () => Math.random().toString(36).substr(2, 6) },
   createdAt: { type: Date, default: Date.now }
 });
 
